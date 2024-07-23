b4dff682b4fb6baae1cbae9c957757e868f400c1
Aodhan Hamilton <hamilton.aodhan@gmail.com>
2024-07-24 00:34:34 +1200

    update clerk sync inngest function

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -2,11 +2,15 @@ import { db } from "@/app/db";
 import { inngest } from "./client";
 import { UserTable } from "@/app/db/schema";
 
-export const insertUser = inngest.createFunction(
-  { id: "add-user-to-db" },
-  { event: "test/user.insert" },
+export const syncUser = inngest.createFunction(
+  { id: "sync-user-from-clerk" }, // ←The 'id' is an arbitrary string used to identify the function in the dashboard
+  { event: "clerk/user.created" }, // ← This is the function's triggering event
   async ({ event }) => {
-    await db.insert(UserTable).values({ userId: "user_5792561", credits: 5 });
-    return { event, body: "User inserted into DB" };
+    const user = event.data; // The event payload's data will be the Clerk User json object
+    const { id, first_name, last_name } = user;
+    const email = user.email_addresses.find(
+      (e: any) => e.id === user.primary_email_address_id
+    ).email;
+    await db.insert(UserTable).values({ userId: id, credits: 5 });
   }
 );
